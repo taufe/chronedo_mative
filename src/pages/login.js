acc2021e4f3d1d7c616eee9952531db97afd7128
your-github-username <taufeeqjadoon777@gmail.com>
2025-03-07 11:50:19 +0500

    login api checking

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,30 +1,87 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import styles from "./Login.module.css";
 import Head from "next/head";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import axios from "axios"; // Import axios
 import userIcon from "../../public/assets/icons/user.png";
 import eyeCloseIcon from "../../public/assets/icons/eyeclose.png";
 import eyeOpenIcon from "../../public/assets/icons/eyeopen.png";
 import checkIcon from "../../public/assets/icons/check.png";
 
-const baseUrl = process.env.NEXT_PUBLIC_BASE_URL
-
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 
 const Login = () => {
-
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [secure, setSecure] = useState(true);
   const [privacyPolicy, setPrivacyPolicy] = useState(false);
   const [terms, setTerms] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
   const router = useRouter();
 
-  const sendData = () => {
-    // Send registration data
-    router.push("/dashboard");
+  // const baseUlr = process.env.NEXT_PUBLIC_BASE_URL;
+
+
+  const handleLogin = async () => {
+
+
+
+    if (!email || !password) {
+      setError("Please fill in all fields.");
+      return;
+    }
+
+    if (!privacyPolicy || !terms) {
+      setError("Please accept the privacy policy and terms.");
+      return;
+    }
+
+    setLoading(true);
+    setError("");
+
+
+    // useEffect(() => {
+    //   const fetchCredential = async () => {
+    //     try {
+    //       await axios.get("https://chronedo.webjerky.com/sanctum/csrf-cookie", {
+    //         withCredentials: true,
+    //       });
+    //     } catch (error) {
+    //       console.error("Error fetching credentials:", error);
+    //     }
+    //   };
+
+    //   fetchCredential();
+    // }, []);
+
+    try {
+      // const res = await axios.get("https://chronedo.webjerky.com/sanctum/csrf-cookie", {
+      //   withCredentials: true,
+      // });
+      const response = await axios.post("https://chronedo.webjerky.com/api/login", {
+        // const response = await axios.post(`${baseUlr}/login`, {
+        email,
+        password,
+      });
+      console.log('response of data', response)
+      if (response.data.success) {
+        localStorage.setItem("token", response.data.data.token);
+        router.push("/dashboard");
+      } else {
+        setError(response.data.message || "Login failed. Please try again.");
+      }
+    } catch (err) {
+      setError(
+        err.response?.data?.message || "An error occurred. Please try again."
+      );
+    } finally {
+      setLoading(false);
+    }
   };
+
   const forgotPassword = () => {
     router.push("/forgotPassword");
   };
@@ -48,6 +105,8 @@ const Login = () => {
           height={50}
         />
         <h2 className={styles.title}>LOG IN</h2>
+
+        {error && <p className={styles.errorMessage}>{error}</p>}
 
         <div className={styles.inputWrapper}>
           <input
@@ -122,8 +181,12 @@ const Login = () => {
           </label>
         </div>
 
-        <button className={styles.submitButton} onClick={sendData}>
-          Log In
+        <button
+          className={styles.submitButton}
+          onClick={handleLogin}
+          disabled={loading}
+        >
+          {loading ? "Logging in..." : "Log In"}
         </button>
 
         <p className={styles.orText}>Or</p>
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -4,6 +4,7 @@ import styles from './Signup.module.css';
 import Head from 'next/head';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import axios from 'axios';
 
 import userIcon from '../../public/assets/icons/user.png';
 import eyeCloseIcon from '../../public/assets/icons/eyeclose.png';
@@ -15,80 +16,92 @@ const SignUp = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [capitalLetters, setCapitalLetters] = useState();
-    const [lower, setLower] = useState(false)
-    const [number, setNumber] = useState(false)
-    const [special, setSpecial] = useState(false)
-    const [length, setLength] = useState(false)
-    const [spaces, setSpaces] = useState(false)
+    const [lower, setLower] = useState(false);
+    const [number, setNumber] = useState(false);
+    const [special, setSpecial] = useState(false);
+    const [length, setLength] = useState(false);
+    const [spaces, setSpaces] = useState(false);
     const [secure, setSecure] = useState(true);
     const [privacyPolicy, setPrivacyPolicy] = useState(false);
     const [terms, setTerms] = useState(false);
+    const [loading, setLoading] = useState(false);
 
     const isValidForm = (password) => {
         if (!isCapital(password)) {
             setCapitalLetters(false);
-        }
-        else {
+        } else {
             setCapitalLetters(true);
         }
         if (!isLower(password)) {
             setLower(false);
-        }
-        else {
+        } else {
             setLower(true);
         }
         if (!isSpecial(password)) {
             setSpecial(false);
-        }
-        else {
+        } else {
             setSpecial(true);
         }
         if (!isNumber(password)) {
             setNumber(false);
-        }
-        else {
+        } else {
             setNumber(true);
         }
         if (password.length < 10 || password.length > 20) {
             setLength(false);
-        }
-        else {
-            setLength(true)
+        } else {
+            setLength(true);
         }
         if (isSpaces(password)) {
             setSpaces(false);
-        }
-        else {
-            setSpaces(true)
+        } else {
+            setSpaces(true);
         }
         setPassword(password);
     };
 
-    const isCapital = labelValue => {
-        const regx = /[A-Z]/;
-        return regx.test(labelValue);
-    };
-    const isLower = labelValue => {
-        const regx = /[a-z]/;
-        return regx.test(labelValue);
-    };
-    const isSpecial = labelValue => {
-        const regx = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
-        return regx.test(labelValue);
+    const isCapital = (labelValue) => /[A-Z]/.test(labelValue);
+    const isLower = (labelValue) => /[a-z]/.test(labelValue);
+    const isSpecial = (labelValue) => /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/.test(labelValue);
+    const isNumber = (labelValue) => /\d/.test(labelValue);
+    const isSpaces = (labelValue) => /\s/.test(labelValue);
+
+    const isValidEmail = (email) => {
+        const regx = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+        return regx.test(email);
     };
 
-    const isNumber = labelValue => {
-        const regx = /\d/;
-        return regx.test(labelValue);
-    };
+    const sendData = async () => {
+        if (!isValidEmail(email)) {
+            alert('Invalid Email', 'Please enter a valid email');
+            return;
+        }
+        if (!(privacyPolicy && terms)) {
+            alert('Terms and conditions', 'Please accept Terms & Conditions/Privacy Policy');
+            return;
+        }
 
-    const isSpaces = labelValue => {
-        const regx = /\s/;
-        return regx.test(labelValue);
-    };
+        setLoading(true);
+        try {
+            const data = {
+                email: email,
+                password: password,
+            };
+            console.log('Sending request to API...');
 
-    const sendData = () => {
-        router.push('/verifyEmail');
+            const response = await axios.post('https://chronedo.webjerky.com/api/register', data);
+            console.log('response--------', response)
+            if (response.data.success) {
+                router.push('/verifyEmail');
+            } else {
+                alert('Registration Failed', response.data.message || 'Something went wrong');
+            }
+        } catch (error) {
+            console.error(error);
+            alert('Error', 'Something went wrong. Please try again.');
+        } finally {
+            setLoading(false);
+        }
     };
 
     return (
@@ -230,7 +243,6 @@ const SignUp = () => {
                     )}
                 </div>
 
-
                 <div className={styles.checkboxContainer}>
                     <label className={styles.checkboxLabel}>
                         <div className={styles.customCheckbox} onClick={() => setPrivacyPolicy(!privacyPolicy)}>
@@ -246,7 +258,9 @@ const SignUp = () => {
                     </label>
                 </div>
 
-                <button className={styles.submitButton} onClick={sendData}>Next</button>
+                <button className={styles.submitButton} onClick={sendData} disabled={loading}>
+                    {loading ? 'Loading...' : 'Next'}
+                </button>
 
                 <p className={styles.orText}>Or</p>
 
@@ -263,18 +277,6 @@ const SignUp = () => {
                     </div>
                     Continue with Google
                 </button>
-                <button className={styles.socialButton} onClick={() => console.log("Google login")}>
-                    <div className={styles.socialRectangle}>
-                        f
-                    </div>
-                    Continue with Facebook
-                </button>
-                <button className={styles.socialButton} onClick={() => console.log("Google login")}>
-                    <div className={styles.socialRectangle}>
-                        t
-                    </div>
-                    Continue with Twitter
-                </button>
 
                 <Link href="/login" passHref>
                     <button className={styles.alreadyHaveAccount}>
@@ -287,3 +289,302 @@ const SignUp = () => {
 };
 
 export default SignUp;
+
+
+
+
+
+
+
+
+
+
+// import React, { useState } from 'react';
+// import Image from 'next/image';
+// import styles from './Signup.module.css';
+// import Head from 'next/head';
+// import Link from 'next/link';
+// import { useRouter } from 'next/router';
+
+// import userIcon from '../../public/assets/icons/user.png';
+// import eyeCloseIcon from '../../public/assets/icons/eyeclose.png';
+// import eyeOpenIcon from '../../public/assets/icons/eyeopen.png';
+// import checkIcon from '../../public/assets/icons/check.png';
+
+// const SignUp = () => {
+//     const router = useRouter();
+//     const [email, setEmail] = useState('');
+//     const [password, setPassword] = useState('');
+//     const [capitalLetters, setCapitalLetters] = useState();
+//     const [lower, setLower] = useState(false)
+//     const [number, setNumber] = useState(false)
+//     const [special, setSpecial] = useState(false)
+//     const [length, setLength] = useState(false)
+//     const [spaces, setSpaces] = useState(false)
+//     const [secure, setSecure] = useState(true);
+//     const [privacyPolicy, setPrivacyPolicy] = useState(false);
+//     const [terms, setTerms] = useState(false);
+
+//     const isValidForm = (password) => {
+//         if (!isCapital(password)) {
+//             setCapitalLetters(false);
+//         }
+//         else {
+//             setCapitalLetters(true);
+//         }
+//         if (!isLower(password)) {
+//             setLower(false);
+//         }
+//         else {
+//             setLower(true);
+//         }
+//         if (!isSpecial(password)) {
+//             setSpecial(false);
+//         }
+//         else {
+//             setSpecial(true);
+//         }
+//         if (!isNumber(password)) {
+//             setNumber(false);
+//         }
+//         else {
+//             setNumber(true);
+//         }
+//         if (password.length < 10 || password.length > 20) {
+//             setLength(false);
+//         }
+//         else {
+//             setLength(true)
+//         }
+//         if (isSpaces(password)) {
+//             setSpaces(false);
+//         }
+//         else {
+//             setSpaces(true)
+//         }
+//         setPassword(password);
+//     };
+
+//     const isCapital = labelValue => {
+//         const regx = /[A-Z]/;
+//         return regx.test(labelValue);
+//     };
+//     const isLower = labelValue => {
+//         const regx = /[a-z]/;
+//         return regx.test(labelValue);
+//     };
+//     const isSpecial = labelValue => {
+//         const regx = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+//         return regx.test(labelValue);
+//     };
+
+//     const isNumber = labelValue => {
+//         const regx = /\d/;
+//         return regx.test(labelValue);
+//     };
+
+//     const isSpaces = labelValue => {
+//         const regx = /\s/;
+//         return regx.test(labelValue);
+//     };
+
+//     const sendData = () => {
+//         router.push('/verifyEmail');
+//     };
+
+//     return (
+//         <>
+//             <Head>
+//                 <title>Sign Up - Your Watch Selling Platform</title>
+//                 <meta name="description" content="Get in touch with us for inquiries about watches." />
+//                 <link rel="icon" href="/favicon.ico" />
+//             </Head>
+//             <div className={styles.innerContainer}>
+//                 <Image
+//                     src={userIcon}
+//                     alt="User"
+//                     className={styles.userImage}
+//                     width={100}
+//                     height={50}
+//                 />
+//                 <h2 className={styles.title}>REGISTER</h2>
+
+//                 <div className={styles.inputWrapper}>
+//                     <input
+//                         type="email"
+//                         className={`${styles.input} ${styles.customPlaceholder}`}
+//                         value={email}
+//                         onChange={(e) => setEmail(e.target.value)}
+//                         placeholder="Email"
+//                     />
+//                 </div>
+
+//                 <div className={styles.inputWrapper}>
+//                     <button onClick={() => setSecure(!secure)} className={styles.eyeButton}>
+//                         <Image
+//                             src={secure ? eyeCloseIcon : eyeOpenIcon}
+//                             alt="Toggle visibility"
+//                             className={styles.eyeIcon}
+//                             width={22}
+//                             height={22}
+//                         />
+//                     </button>
+//                     <input
+//                         type={secure ? "password" : "text"}
+//                         className={`${styles.input} ${styles.customPlaceholder}`}
+//                         style={{ marginBottom: '20px' }}
+//                         value={password}
+//                         onChange={(e) => isValidForm(e.target.value)}
+//                         placeholder="Password"
+//                     />
+//                 </div>
+
+//                 <div className={styles.conditionsContainer}>
+//                     {capitalLetters ? (
+//                         <div className={styles.condition}>
+//                             <Image
+//                                 src={checkIcon}
+//                                 alt="Check"
+//                                 className={styles.checkIcon}
+//                                 width={17}
+//                                 height={17}
+//                             />
+//                             <p>Capital letters {"[ABC]"}</p>
+//                         </div>
+//                     ) : (
+//                         <div className={styles.condition}>
+//                             <div className={styles.uncheckedIcon}></div>
+//                             <p className={styles.errorText}>Capital letters {"[ABC]"}</p>
+//                         </div>
+//                     )}
+
+//                     {lower ? (
+//                         <div className={styles.condition}>
+//                             <Image
+//                                 src={checkIcon}
+//                                 alt="Check"
+//                                 className={styles.checkIcon}
+//                                 width={17}
+//                                 height={17}
+//                             />
+//                             <p>Lowercase letters {"[abc]"}</p>
+//                         </div>
+//                     ) : (
+//                         <div className={styles.condition}>
+//                             <div className={styles.uncheckedIcon}></div>
+//                             <p className={styles.errorText}>Lowercase letters {"[abc]"}</p>
+//                         </div>
+//                     )}
+
+//                     {spaces ? (
+//                         <div className={styles.condition}>
+//                             <Image
+//                                 src={checkIcon}
+//                                 alt="Check"
+//                                 className={styles.checkIcon}
+//                                 width={17}
+//                                 height={17}
+//                             />
+//                             <p>No spaces</p>
+//                         </div>
+//                     ) : (
+//                         <div className={styles.condition}>
+//                             <div className={styles.uncheckedIcon}></div>
+//                             <p className={styles.errorText}>No spaces</p>
+//                         </div>
+//                     )}
+
+//                     {(special || number) ? (
+//                         <div className={styles.condition}>
+//                             <Image
+//                                 src={checkIcon}
+//                                 alt="Check"
+//                                 className={styles.checkIcon}
+//                                 width={17}
+//                                 height={17}
+//                             />
+//                             <p>A digit {"[123] / special character [! $ @]"}</p>
+//                         </div>
+//                     ) : (
+//                         <div className={styles.condition}>
+//                             <div className={styles.uncheckedIcon}></div>
+//                             <p className={styles.errorText}>A digit {"[123] / special character [! $ @]"}</p>
+//                         </div>
+//                     )}
+
+//                     {length ? (
+//                         <div className={styles.condition}>
+//                             <Image
+//                                 src={checkIcon}
+//                                 alt="Check"
+//                                 className={styles.checkIcon}
+//                                 width={17}
+//                                 height={17}
+//                             />
+//                             <p>10 - 20 letters</p>
+//                         </div>
+//                     ) : (
+//                         <div className={styles.condition}>
+//                             <div className={styles.uncheckedIcon}></div>
+//                             <p className={styles.errorText}>10 - 20 letters</p>
+//                         </div>
+//                     )}
+//                 </div>
+
+
+//                 <div className={styles.checkboxContainer}>
+//                     <label className={styles.checkboxLabel}>
+//                         <div className={styles.customCheckbox} onClick={() => setPrivacyPolicy(!privacyPolicy)}>
+//                             {privacyPolicy && <Image src="/assets/icons/on.png" alt="Checked" width={20} height={20} />}
+//                         </div>
+//                         I accept the privacy policy.
+//                     </label>
+//                     <label className={styles.checkboxLabel}>
+//                         <div className={styles.customCheckbox} onClick={() => setTerms(!terms)}>
+//                             {terms && <Image src="/assets/icons/on.png" alt="Checked" width={20} height={20} />}
+//                         </div>
+//                         I accept the terms and conditions.
+//                     </label>
+//                 </div>
+
+//                 <button className={styles.submitButton} onClick={sendData}>Next</button>
+
+//                 <p className={styles.orText}>Or</p>
+
+//                 <button className={styles.socialButton} onClick={() => console.log("LinkedIn login")}>
+//                     <div className={styles.socialRectangle}>
+//                         in
+//                     </div>
+//                     Continue with LinkedIn
+//                 </button>
+
+//                 <button className={styles.socialButton} onClick={() => console.log("Google login")}>
+//                     <div className={styles.socialRectangle}>
+//                         G
+//                     </div>
+//                     Continue with Google
+//                 </button>
+//                 <button className={styles.socialButton} onClick={() => console.log("Google login")}>
+//                     <div className={styles.socialRectangle}>
+//                         f
+//                     </div>
+//                     Continue with Facebook
+//                 </button>
+//                 <button className={styles.socialButton} onClick={() => console.log("Google login")}>
+//                     <div className={styles.socialRectangle}>
+//                         t
+//                     </div>
+//                     Continue with Twitter
+//                 </button>
+
+//                 <Link href="/login" passHref>
+//                     <button className={styles.alreadyHaveAccount}>
+//                         Already have an account?
+//                     </button>
+//                 </Link>
+//             </div>
+//         </>
+//     );
+// };
+
+// export default SignUp;
